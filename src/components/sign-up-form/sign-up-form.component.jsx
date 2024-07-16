4837202beb13caa8e65d1693ac7cfb1078a35e63
law973 <lawright379@gmail.com>
2024-07-16 12:13:19 -0300

    made a generalized form input component

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 
+import FormInput from '../form-input/form-input.component';
+
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 
 const defaultFormFields = {
@@ -54,8 +56,8 @@ const SignUpForm = () => {
         <div>
             <h1>Sign up with your email and password</h1>
             <form onSubmit={handleSubmit}>
-                <label>Display Name</label>
-                <input 
+                <FormInput
+                    label="Display Name" 
                     type='text' 
                     required 
                     onChange={handleChange} 
@@ -63,14 +65,32 @@ const SignUpForm = () => {
                     value={displayName} 
                 />
 
-                <label>Email</label>
-                <input type='email' required onChange={handleChange} name='email' value={email} />
+                <FormInput 
+                    label="Email"
+                    type='email' 
+                    required 
+                    onChange={handleChange} 
+                    name='email' 
+                    value={email} 
+                />
 
-                <label>Password</label>
-                <input type='password' required onChange={handleChange} name='password' value={password} />
+                <FormInput 
+                    label="Password"
+                    type='password' 
+                    required 
+                    onChange={handleChange} 
+                    name='password' 
+                    value={password} 
+                />
 
-                <label>Confirm Password</label>
-                <input type='password' required onChange={handleChange} name='confirmPassword' value={confirmPassword} />
+                <FormInput 
+                    label="Confirm Password"
+                    type='password' 
+                    required 
+                    onChange={handleChange} 
+                    name='confirmPassword' 
+                    value={confirmPassword} 
+                />
                 <button type='submit'>Sign Up</button>
             </form>
         </div>
