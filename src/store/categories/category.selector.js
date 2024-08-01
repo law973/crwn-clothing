a513aa3ee7322a10e26d3dabcfaad4cf087344bc
law973 <lawright379@gmail.com>
2024-08-01 10:15:53 -0300

    migrated cart context to redux

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,6 @@
 import { createSelector } from "reselect";
 
-const selectCategoryReducer = (state) => state.categories;
+const selectCategoryReducer = state => state.categories;
 
 export const selectCategories = createSelector(
     [selectCategoryReducer],
