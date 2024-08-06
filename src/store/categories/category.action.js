4ef31f2a7bda760845cb6773d85789fa8ea1c49e
law973 <lawright379@gmail.com>
2024-08-06 12:50:54 -0300

    starting to set up stripe

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,6 @@
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
 
-import { createAction } from "../../utils/firebase/reducer/reducer.utils";
+import { createAction } from "../../utils/reducer/reducer.utils";
 
 export const fetchCategoriesStart = () => 
     createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
