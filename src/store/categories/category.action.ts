97abee57ba1785e5f30ab2248674fffa0e470d20
law973 <lawright379@gmail.com>
2024-08-10 11:06:18 -0300

    redux cart files have been typed

--- src/store/categories/category.action.ts
+++ src/store/categories/category.action.ts
@@ -1,6 +1,11 @@
 import { CATEGORIES_ACTION_TYPES, Category } from "./category.types";
 
-import { createAction, Action, ActionWithPayload } from "../../utils/reducer/reducer.utils";
+import { 
+    createAction, 
+    Action, 
+    ActionWithPayload,
+    withMatcher
+} from "../../utils/reducer/reducer.utils";
 
 export type FetchCategoriesStart = 
     Action<CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START>;
@@ -17,16 +22,11 @@ export type FetchCategoriesFailed =
         Error
     >;
 
-export type CategoryAction =
-    | FetchCategoriesStart
-    | FetchCategoriesSuccess
-    | FetchCategoriesFailed;
+export const fetchCategoriesStart = withMatcher((): FetchCategoriesStart => 
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START));
 
-export const fetchCategoriesStart = (): FetchCategoriesStart => 
-    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+export const fetchCategoriesSuccess = withMatcher((categoriesArray: Category[]): FetchCategoriesSuccess =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray));
 
-export const fetchCategoriesSuccess = (categoriesArray: Category[]): FetchCategoriesSuccess =>
-    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
-
-export const fetchCategoriesFailed = (error: Error): FetchCategoriesFailed =>
-    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error);
+export const fetchCategoriesFailed = withMatcher((error: Error): FetchCategoriesFailed =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED, error));
