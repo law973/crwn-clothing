bb2093e6bd808c05ce7fa889eb4e878b7e3d182e
law973 <lawright379@gmail.com>
2024-08-11 11:03:59 -0300

    typed user reducer and user selector

---
+++ src/store/user/user.reducer.ts
@@ -0,0 +1,33 @@
+import { AnyAction } from "redux";
+
+import { signInSuccess, signOutSuccess, signOutFailed, signInFailed, signUpFailed } from "./user.action";
+
+import { UserData } from "../../utils/firebase/firebase.utils";
+
+export type UserState = {
+    readonly currentUser: UserData | null;
+    readonly isLoading: boolean;
+    readonly error: Error | null;
+};
+
+const USER_INITIAL_STATE: UserState = {
+    currentUser: null,
+    isLoading: false,
+    error: null,
+};
+
+export const userReducer = (state = USER_INITIAL_STATE, action: AnyAction): UserState => {
+    if(signInSuccess.match(action)) {
+        return { ...state, currentUser: action.payload };
+    }
+
+    if(signOutSuccess.match(action)) {
+        return { ...state, currentUser: null };
+    }
+
+    if(signInFailed.match(action) || signUpFailed.match(action) || signOutFailed.match(action)) {
+        return { ...state, error: action.payload }; 
+    }
+
+    return state;
+};
